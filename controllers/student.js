4808d600cab5bd7a4ef269254522834a39178e1c
antonrenzobenedict <antonrenzobenedict@gmail.com>
2015-09-10 13:50:03 +0800

    More Test Cases

--- controllers/student.js
+++ controllers/student.js
@@ -1,6 +1,8 @@
 var db = require(__dirname + '/../lib/mysql');
 
 exports.find = function(req,res,next){
+	console.log("find()");
+	console.log(req.ip + " find()");
 	db.query("SELECT * FROM student",
 		function (err,rows){
 			if(err) return next(err);
@@ -8,13 +10,17 @@ exports.find = function(req,res,next){
 		});
 };
 exports.insert = function(req,res,next){
+	
 	db.query("INSERT INTO student(studno,name)VALUES(?,?)",[req.body.studno,req.body.name],
 		function (err,rows){
 			if(err) return next(err);
 			res.send(rows);
+			console.log(res.body + " find()");
 		});
 };
 exports.findOne = function(req,res,next){
+	console.log("findOne()");
+	console.log(req.ip + " find()");
 	db.query("SELECT * FROM student WHERE id=?",[req.params.id],
 		function (err,rows){
 			if(err) return next(err);
